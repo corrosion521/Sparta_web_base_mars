c36968f4d06a8db25944c3e4135c4cc5c39d8673
corrosion521@naver.com <corrosion521@naver.com>
2023-02-03 17:05:40 +0900

    GET 기능: DB에 있는 내용 새로고침마다 클라이언트에 업데이트 /서버, 프론트 완료

--- app.py
+++ app.py
@@ -28,7 +28,10 @@ def web_mars_post():
 
 @app.route("/mars", methods=["GET"])
 def web_mars_get():
-    return jsonify({'msg': 'GET 연결 완료!'})
+    #db에서 다 가져오자
+    order_list = list(db.mars.find({}, {'_id' : False}))#조건 없고, id 없이
+
+    return jsonify({'orders':order_list})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
